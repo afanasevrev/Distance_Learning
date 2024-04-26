e01e583bbad85f8577b4173fedd9cbc9e194649b
revoly afanasev <rewoly@mail.ru>
2024-04-27 00:48:28 +0900

    for student

--- Client/src/main/java/com/example/client/ClientController.java
+++ Client/src/main/java/com/example/client/ClientController.java
@@ -3,7 +3,7 @@ package com.example.client;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ClientController {
     @FXML
     private Label welcomeText;
 
