acc067bcfd0b826a9a48aa7d2286599c2b81082a
revoly afanasev <rewoly@mail.ru>
2024-04-29 00:59:14 +0900

    for student

--- Client/src/main/java/com/example/client/ClientApplication.java
+++ Client/src/main/java/com/example/client/ClientApplication.java
@@ -6,12 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
+/**
+ * Точка входа в приложение
+ */
 public class ClientApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("sign_in.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Distance learning!");
         stage.setScene(scene);
         stage.show();
