ecafe654aaa30078dc2dc47fe876877a919364a8
revoly afanasev <rewoly@mail.ru>
2024-04-28 19:18:38 +0900

    for student

--- Client/src/main/java/com/example/client/ListOfMaterial.java
+++ Client/src/main/java/com/example/client/ListOfMaterial.java
@@ -1,10 +1,27 @@
 package com.example.client;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import lombok.Getter;
 import lombok.Setter;
-
+/**
+ * Класс для учебных материалов
+ */
 @Setter
 @Getter
 public class ListOfMaterial {
-
+    //Порядковый номер учебного материала
+    private StringProperty id;
+    //Наименование самого материала
+    private StringProperty materialName;
+    public ListOfMaterial(String id, String materialName) {
+        this.id = new SimpleStringProperty(this, "id", id);
+        this.materialName = new SimpleStringProperty(this, "materialName", materialName);
+    }
+    public StringProperty idProperty() {
+        return id;
+    }
+    public StringProperty materialNameProperty() {
+        return materialName;
+    }
 }
