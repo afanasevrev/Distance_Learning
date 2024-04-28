471e313b655c80d1d5786f768e14bbc012c9b3bb
revoly afanasev <rewoly@mail.ru>
2024-04-28 23:38:06 +0900

    for student

--- Client/src/main/java/com/example/client/ListOfVideo.java
+++ Client/src/main/java/com/example/client/ListOfVideo.java
@@ -12,5 +12,31 @@ public class ListOfVideo {
         this.videoName = new SimpleStringProperty(this, "videoName", videoName);
         this.linkInVideo = new SimpleStringProperty(this, "linkInVideo", linkInVideo);
     }
-    
+    public String getId() {
+        return id.get();
+    }
+    public StringProperty idProperty() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id.set(id);
+    }
+    public String getVideoName() {
+        return videoName.get();
+    }
+    public StringProperty videoNameProperty() {
+        return videoName;
+    }
+    public void setVideoName(String videoName) {
+        this.videoName.set(videoName);
+    }
+    public String getLinkInVideo() {
+        return linkInVideo.get();
+    }
+    public StringProperty linkInVideoProperty() {
+        return linkInVideo;
+    }
+    public void setLinkInVideo(String linkInVideo) {
+        this.linkInVideo.set(linkInVideo);
+    }
 }
