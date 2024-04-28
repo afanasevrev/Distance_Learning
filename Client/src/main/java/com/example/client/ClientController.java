4ac40c5088212c1924b4072fb52e27a3855a5faa
revoly afanasev <rewoly@mail.ru>
2024-04-28 21:58:44 +0900

    for student

--- Client/src/main/java/com/example/client/ClientController.java
+++ Client/src/main/java/com/example/client/ClientController.java
@@ -1,16 +1,29 @@
 package com.example.client;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-
+/**
+ * Класс контроллер для взаимодействия с основной формой FX
+ */
 public class ClientController {
+    //Элементы вкладки "Учебные материалы"
     @FXML
     private Button updateList = new Button();
     @FXML
     private Button downloadMaterial = new Button();
     @FXML
-    TableView tableViewListOfMaterial = new TableView();
+    private TableView tableViewListOfMaterial = new TableView();
+    private ObservableList<ListOfMaterial> listOfMaterialData = FXCollections.<ListOfMaterial>observableArrayList();
+    @FXML
+    TableColumn<ListOfMaterial, String> id = new TableColumn<ListOfMaterial, String>("№");
+    @FXML
+    TableColumn<ListOfMaterial, String> materialName = new TableColumn<ListOfMaterial, String>("Наименование");
+    //_____________________________________________________________________________________________________________//
+    //_____________________________________________________________________________________________________________//
+    //Элементы вкладки "Видеоуроки"
 
 }
