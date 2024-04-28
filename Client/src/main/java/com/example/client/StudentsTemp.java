7b1092f503f49963186c4aa2d9628f3f9db7964a
revoly afanasev <rewoly@mail.ru>
2024-04-29 00:43:12 +0900

    for student

--- Client/src/main/java/com/example/client/StudentsTemp.java
+++ Client/src/main/java/com/example/client/StudentsTemp.java
@@ -1,6 +1,20 @@
 package com.example.client;
+
+import lombok.Getter;
+import lombok.Setter;
 /**
  * Класс для взаимодействия с сервером
  */
+@Setter
+@Getter
 public class StudentsTemp {
+    private int id;
+    private String name;
+    private String surname;
+    public StudentsTemp(){}
+    public StudentsTemp(int id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
 }
