3fe7e184d36b042cc02f8e8feb6b1b1f33935f5c
revoly afanasev <rewoly@mail.ru>
2024-04-28 18:37:58 +0900

    for student

--- Client/src/main/java/com/example/client/SignInController.java
+++ Client/src/main/java/com/example/client/SignInController.java
@@ -1,5 +1,31 @@
 package com.example.client;
 
-public class SignInController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
+public class SignInController {
+    @FXML
+    private TextField login = new TextField();
+    @FXML
+    private PasswordField password = new PasswordField();
+    @FXML
+    private Button signIn = new Button();
+    @FXML
+    private TextField surname = new TextField();
+    @FXML
+    private TextField name = new TextField();
+    @FXML
+    private TextField patronymic = new TextField();
+    @FXML
+    private TextField loginForRegistration = new TextField();
+    @FXML
+    private TextField passwordForRegistration = new TextField();
+    @FXML
+    private Button registration = new Button();
+    @FXML
+    private TextArea logs = new TextArea();
+    public SignInController(){}
 }
