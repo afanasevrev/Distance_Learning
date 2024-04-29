8eca7dcc77896cdf096c3201f848dcc1f58be8ef
revoly afanasev <rewoly@mail.ru>
2024-04-29 23:05:37 +0900

    for student

--- Server/src/main/java/com/example/Server/controllers/ServerController.java
+++ Server/src/main/java/com/example/Server/controllers/ServerController.java
@@ -11,7 +11,7 @@ public class ServerController {
         return "Дистанционное обучение для охранников 4-5-6 разряда";
     }
     @GetMapping("/authenticate/{login}&{password}")
-    private Direction getAuthentication(@PathVariable String login,@PathVariable String password) {
+    private Direction getAuthentication(@PathVariable String login, @PathVariable String password) {
 
         return Direction.AUTHENTICATED;
     }
