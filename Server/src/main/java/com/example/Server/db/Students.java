420dca98b2e7be3926aa04a56d27e77c9d73f00c
revoly afanasev <rewoly@mail.ru>
2024-04-29 17:32:46 +0900

    for student

---
+++ Server/src/main/java/com/example/Server/db/Students.java
@@ -0,0 +1,33 @@
+package com.example.Server.db;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+@Entity
+@Table(name = "students")
+public class Students {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "patronymic")
+    private String patronymic;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+    public Students(){}
+    public Students(String surname, String name, String patronymic, String login, String password) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.login = login;
+        this.password = password;
+    }
+}
