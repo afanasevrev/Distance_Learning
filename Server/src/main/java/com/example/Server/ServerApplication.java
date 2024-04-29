045e508d02d085d0c8ba8df707bfdd973c4f2d59
revoly afanasev <rewoly@mail.ru>
2024-04-29 23:42:01 +0900

    for student

--- Server/src/main/java/com/example/Server/ServerApplication.java
+++ Server/src/main/java/com/example/Server/ServerApplication.java
@@ -1,11 +1,13 @@
 package com.example.Server;
 
+import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
 	public static void main(String[] args) {
+		BasicConfigurator.configure();
 		SpringApplication.run(ServerApplication.class, args);
 	}
 }
--- Server/src/main/java/com/example/Server/controllers/ServerController.java
+++ Server/src/main/java/com/example/Server/controllers/ServerController.java
@@ -1,9 +1,16 @@
 package com.example.Server.controllers;
 
 import com.example.Server.Direction;
+import com.example.Server.db.Admins;
+import com.example.Server.hibernate.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 public class ServerController {
     @GetMapping("/")
@@ -15,5 +22,28 @@ public class ServerController {
 
         return Direction.AUTHENTICATED;
     }
+    /**
+     *
+     * @return
+     */
+    private boolean getAdmins() {
+        boolean result = false;
+        List<Admins> admins;
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // Старт транзакции
+            transaction = session.beginTransaction();
+            admins = session.createQuery("from Admins", Admins.class).getResultList();
+            // Коммит транзакции
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            result = false;
+        }
 
+        return result;
+    }
 }
