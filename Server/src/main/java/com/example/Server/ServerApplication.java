bb20aaf23a1c09f1861e21519cb5ca06a19f7dde
revoly afanasev <rewoly@mail.ru>
2024-05-01 18:46:00 +0900

    for student

--- Server/src/main/java/com/example/Server/ServerApplication.java
+++ Server/src/main/java/com/example/Server/ServerApplication.java
@@ -3,7 +3,9 @@ package com.example.Server;
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Точка запуска сервера
+ */
 @SpringBootApplication
 public class ServerApplication {
 	public static void main(String[] args) {
