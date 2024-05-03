74a0d3d2ebee519199015208f68e425d76d34873
unknown <rewoly@mail.ru>
2024-05-03 15:48:24 +0900

    first commit

--- Client/src/main/java/com/example/client/Variables.java
+++ Client/src/main/java/com/example/client/Variables.java
@@ -1,4 +1,6 @@
 package com.example.client;
+
+import java.security.SecureRandom;
 /**
  * В этом классе будем хранить статические переменные
  */
@@ -7,5 +9,14 @@ public class Variables {
     public static String ip_server = "localhost";
     //Порт сервера
     public static String port_server = "8080";
+    private static final String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final SecureRandom RANDOM = new SecureRandom();
+    public static String generateRandomFileName(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(LETTERS.charAt(RANDOM.nextInt(LETTERS.length())));
+        }
+        return sb.toString();
+    }
 }
 
