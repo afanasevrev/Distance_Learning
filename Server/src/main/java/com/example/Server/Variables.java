2fd635edc0f3afa4358496bf0203a1c5c88f553c
unknown <rewoly@mail.ru>
2024-05-03 15:47:18 +0900

    first commit

---
+++ Server/src/main/java/com/example/Server/Variables.java
@@ -0,0 +1,17 @@
+package com.example.Server;
+
+import java.security.SecureRandom;
+/**
+ * Класс для статических переменных
+ */
+public class Variables {
+    private static final String LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final SecureRandom RANDOM = new SecureRandom();
+    public static String generateRandomFileName(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(LETTERS.charAt(RANDOM.nextInt(LETTERS.length())));
+        }
+        return sb.toString();
+    }
+}
