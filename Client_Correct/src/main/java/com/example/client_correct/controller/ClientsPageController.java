b4b59b987a5187e9b6e19a5f40e17d6ec388d2f3
revoly afanasev <rewoly@mail.ru>
2024-06-06 01:09:11 +0900

    for student

--- Client_Correct/src/main/java/com/example/client_correct/controller/ClientsPageController.java
+++ Client_Correct/src/main/java/com/example/client_correct/controller/ClientsPageController.java
@@ -1,4 +1,19 @@
 package com.example.client_correct.controller;
 
+import com.example.client_correct.material.ListOfMaterials;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+/**
+ * Класс контроллер для взаимодействия с формой "clients_page.fxml"
+ */
 public class ClientsPageController {
+    //-----------------------------------------------------------------------------//
+    //Элементы вкладки "Учебные материалы"
+    private String valueOfMaterial;
+    @FXML
+    private TableView<ListOfMaterials> tableViewMaterials = new TableView<ListOfMaterials>();
+    private ObservableList<ListOfMaterials> listOfMaterialsData = FXCollections.<ListOfMaterials>observableArrayList();
+    
 }
---
+++ Client_Correct/src/main/java/com/example/client_correct/material/ListOfMaterials.java
@@ -0,0 +1,4 @@
+package com.example.client_correct.material;
+
+public class ListOfMaterials {
+}
