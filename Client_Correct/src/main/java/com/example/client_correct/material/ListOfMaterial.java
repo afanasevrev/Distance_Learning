a4f298b321c5020ecd1410a4f39e5e0e539554b8
revoly afanasev <rewoly@mail.ru>
2024-06-06 01:35:04 +0900

    for student

--- Client_Correct/src/main/java/com/example/client_correct/material/ListOfMaterial.java
+++ Client_Correct/src/main/java/com/example/client_correct/material/ListOfMaterial.java
@@ -1,4 +1,36 @@
 package com.example.client_correct.material;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+/**
+ * Класс для учебных материалов
+ * тесно взаимосвязан с таблицей FX во вкладке "Учебный материал"
+ */
 public class ListOfMaterial {
+    //Порядковый номер учебного материала
+    public StringProperty id;
+    //Наименование самого материала
+    public StringProperty materialName;
+    public ListOfMaterial(String id, String materialName) {
+        this.id = new SimpleStringProperty(this, "id", id);
+        this.materialName = new SimpleStringProperty(this, "materialName", materialName);
+    }
+    public String getId() {
+        return id.get();
+    }
+    public void setId(String id) {
+        this.id.set(id);
+    }
+    public String getMaterialName() {
+        return materialName.get();
+    }
+    public void setMaterialName(String materialName) {
+        this.materialName.set(materialName);
+    }
+    public StringProperty idProperty() {
+        return id;
+    }
+    public StringProperty materialNameProperty() {
+        return materialName;
+    }
 }
