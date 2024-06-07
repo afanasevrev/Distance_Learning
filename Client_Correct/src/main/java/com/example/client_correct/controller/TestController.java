d689485b61ae94dd9dd0883d2f0f306ba911f104
revoly afanasev <rewoly@mail.ru>
2024-06-07 18:29:08 +0900

    for student

--- Client_Correct/src/main/java/com/example/client_correct/controller/TestController.java
+++ Client_Correct/src/main/java/com/example/client_correct/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.example.client_correct.controller;
 
+import com.example.client_correct.test.TestSecurity;
+import com.example.client_correct.variables.Variables;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -8,8 +10,12 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import org.apache.log4j.Logger;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
 /**
  * Класс для загрузки теста в форму
  */
@@ -43,6 +49,19 @@ public class TestController implements Initializable {
      */
     @FXML
     private void setButtonStartTest() {
-        
+        String url_start_test = "http://" + Variables.ip_server + ":" +Variables.port_server + "/getTestCategory";
+        String url_category = "4";
+        if (Variables.category == 4) {
+            url_category = "4";
+        } else if (Variables.category == 5) {
+            url_category = "5";
+        } else if (Variables.category == 6) {
+            url_category = "6";
+        } else {
+            url_category = "4";
+        }
+        url_start_test = url_start_test + url_category;
+        ResponseEntity<List<TestSecurity>> response = restTemplate.exchange(url_start_test, HttpMethod.GET, null, new ParameterizedTypeReference<List<TestSecurity>>(){});
+        logger.info(response.getBody().get(0).getQuestion());
     }
 }
--- Client_Correct/src/main/java/module-info.java
+++ Client_Correct/src/main/java/module-info.java
@@ -4,10 +4,12 @@ module com.example.client_correct {
     requires log4j;
     requires static lombok;
     requires spring.web;
+    requires spring.core;
 
 
     opens com.example.client_correct to javafx.fxml;
     exports com.example.client_correct;
     exports com.example.client_correct.controller;
+    exports com.example.client_correct.test;
     opens com.example.client_correct.controller to javafx.fxml;
 }
