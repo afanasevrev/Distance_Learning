74d1c94ebad5589feb865296a43b314677be71c1
revoly afanasev <rewoly@mail.ru>
2024-06-08 22:32:03 +0900

    for student

---
+++ Client_Correct/src/main/java/com/example/client_correct/authorization/Login.java
@@ -0,0 +1,27 @@
+package com.example.client_correct.authorization;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class Login {
+    private String login;
+    private String password;
+    private int category;
+    private String type;
+    public Login() {}
+    public Login(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+    public Login(int category, String type) {
+        this.category = category;
+        this.type = type;
+    }
+    public Login(String login, String password, int category, String type) {
+        this.login = login;
+        this.password = password;
+        this.category = category;
+        this.type = type;
+    }
+}
